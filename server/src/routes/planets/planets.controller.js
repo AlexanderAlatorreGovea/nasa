61f7d5fea429d187fcd2d23616e275867c69e993
Alexander <Alexander_alatorre1993@hotmail.com>
2022-10-07 20:10:07 -0500

    added get all planets

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,7 +1,7 @@
-const planets = require("../../models/planets.model");
+const { planets } = require("../../models/planets.model");
 
 function getAllPlanets(req, res) {
-  return res.status(200).json(planets)
+  return res.status(200).json(planets);
 }
 
 module.exports = {
--- server/src/server.js
+++ server/src/server.js
@@ -2,9 +2,17 @@ const http = require("http");
 
 const app = require("./app");
 
+const { loadPlanetsData } = require("./models/planets.model");
+
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 8000;
-server.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
-});
+async function startServer() {
+  await loadPlanetsData();
+
+  const PORT = process.env.PORT || 8000;
+  server.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
+  });
+}
+
+startServer()
